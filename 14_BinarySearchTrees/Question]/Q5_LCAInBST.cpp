8dcc40e6dd3519f9353e4e6c97e53c75fd4996fa
kratikesh18 <kartikpachkawade@gmail.com>
2023-05-19 20:29:11 +0530

    more Questions on BST

---
+++ 14_BinarySearchTrees/Question]/Q5_LCAInBST.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+class Node {
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int data) {
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+Node* buildTree() {
+    int data;
+
+    cin >> data;
+    if (data == -1) {
+        return NULL;
+    }
+
+    Node* root = new Node(data);
+    cout << "\nEnter Data for the left part of " << data << " node: ";
+    root->left = buildTree();
+
+    cout << "\nEnter Data for the right part of " << data << " node: ";
+    root->right = buildTree();
+
+    return root;
+}
+int main(){
+    Node* root = NULL;
+    cout << "Enter the root Node: ";
+    root = buildTree();
+
+return 0;
+}
