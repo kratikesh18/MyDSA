8a76be43efce27538d51f630b0f85e1ac5c7d3a6
kratikesh18 <kartikpachkawade@gmail.com>
2023-06-03 15:18:28 +0530

    New heap Questions added

---
+++ 15_Heaps/Questions (Heaps)/Q4_ConvertBSTtoHeap.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //this is imcomplete question 
+    //the structure of the tree remains same just convert into the heap
+    
+    //store InOrder
+    // and insert Post Order
+
+
+return 0;
+}
---
+++ 15_Heaps/Questions (Heaps)/Q5_IsCBTorNot.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //is the
+
+return 0;
+}
