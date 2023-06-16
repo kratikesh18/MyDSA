33b2895e14b754e65a3fbfb9f78099334aec05b6
kratikesh18 <kartikpachkawade@gmail.com>
2023-06-16 14:53:00 +0530

    painting fence's BottomUp and SpaceOptimised solution added

--- 18_Dynamic Programming/Q5_PaintingFence.cpp
+++ 18_Dynamic Programming/Q5_PaintingFence.cpp
@@ -32,6 +32,39 @@ int solveUsingTopDown(int n  , int k ,vector<int>&dp){
     return dp[n];
 
 }
+
+int solveUsingBottomUp(int n  ,int k){
+    //create array
+    vector<int>dp(n+1, 0);
+
+    //analizing base cases
+    dp[1] = k;
+    dp[2] = (k+k *(k-1));
+
+    //step 3: analizing the flow 
+    for(int  i =3; i <= n ; i++){
+        dp[i] = (dp[i-2] + dp[i-1])*(k-1);      //yaha i ki jagah n likhega "___FASEGA___"
+
+    }
+    return dp[n];
+}
+
+
+
+    //space optimised approch 
+    int SolveSpaceOptimised(int n , int k){
+        int prev2 = k;
+        int prev1 = (k+k*(k-1));
+
+        for(int i = 3 ; i <= n ; i++){
+            int curr = (prev2 + prev1)*(k-1);
+
+            // ATTENTION while SHIFTING TTHE POINTERS
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
 int main(){
     int n = 4;
     int k = 3;
@@ -40,7 +73,9 @@ int main(){
     
     // int ans =solveUsingRec(n , k);
 
-    int ans = solveUsingTopDown(n  , k , dp);
+    // int ans = solveUsingTopDown(n  , k , dp);
+    // int ans = solveUsingBottomUp(3  , 2 );
+    int ans = SolveSpaceOptimised(n  , k );
     cout<<"ans is: "<<ans<<endl;
 
 return 0;
